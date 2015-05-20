47aa27b8c52edf67ab584c7d75d2940d27006ec5
russell-mason <public@russellmason.com>
2015-05-20 21:03:50 +0100

    Init

---
+++ GitHubExperiments/Log.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GitHubExperiments
+{
+	public class Log
+	{
+		public void Output(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
---
+++ GitHubExperiments/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitHubExperiments
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var log = new Log();
+
+			log.Output("Test");
+		}
+	}
+}
